e1fae4c63903d648d730155aed6e9be14189ed4f
Onlygra62 <wmsnmc02@gmail.com>
2026-08-21 12:07:08 -0500

    feat: Ingest

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import os
+from src.services.ingest import DataFrame
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
+
+@app.get("/ingest")
+async def root():
+    data_frame = DataFrame(os.path.abspath(r"src\services\data.csv"))
+    return data_frame.dict()
--- backend/src/services/ingest.py
+++ backend/src/services/ingest.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import os
 
-data_frame = pd.read_csv('data.csv')
-
-print(data_frame)
+class DataFrame():
+    def __init__(self, path):
+        self.data_frame = pd.read_csv(path)
+        
+    def json(self):
+        return self.data_frame.to_json(orient="records", lines=True)[1:-1].replace("},{", "} {")
+    
+    def str(self):
+        return self.data_frame.to_string()
+    
+    def dict(self):
+        return self.data_frame.to_dict(orient="records")
